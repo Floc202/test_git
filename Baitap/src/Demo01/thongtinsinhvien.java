c922b2dedca676088b1644efb70fe826a80a41d0
Floc202 <locloc2002nk@gmail.com>
2023-05-09 23:07:08 +0700

    Test01

---
+++ Baitap/src/Demo01/thongtinsinhvien.java
@@ -0,0 +1,18 @@
+package Demo01;
+
+
+public class thongtinsinhvien {
+    public static void main(String[] args) {
+        student sv1=new studen();
+        sv1.code="SE161090";
+        sv1.name="Nguyen Tuan Loc";
+        sv1.gpa=8.5f;
+        sv1.viewgpa();
+        
+        student sv2=new studen();
+        sv2.code="SE171090";
+        sv2.name="Nguyen Van An";
+        sv2.gpa=8.0f;
+        sv2.viewgpa();
+    }
+}
---
+++ Baitap/src/Studen.java
@@ -0,0 +1,12 @@
+
+public class Studen{
+    
+            private String code;
+            private String name;
+            private float  gpa;
+                       
+    
+    public void viewgpa(){
+            System.out.println(code +" - "+" - "+ gpa);
+        }
+}
